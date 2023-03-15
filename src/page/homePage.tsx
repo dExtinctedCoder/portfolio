7cb637269b2e953c6cfb35555180599e47e83b59
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-16 00:38:28 +0100

    Home page(route) component

---
+++ src/page/homePage.tsx
@@ -0,0 +1,13 @@
+import About from "../components/about"
+import Hero from "../components/hero"
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <About />
+    </div>
+  )
+}
+
+export default HomePage
