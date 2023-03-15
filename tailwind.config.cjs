c5549b21f56ce492f6c088adae6253f229df8e14
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-16 00:36:42 +0100

    add custom themes to config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,17 @@
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      'black': '#1B1B1B',
+      'white': '#FFFFFF',
+      'green-light': '#C8FF00',
+      'gold-light': '#E89300',
+    },
+    fontFamily: {
+      pry: [],
+      sec: [],
+    },
     extend: {},
   },
   plugins: [],
