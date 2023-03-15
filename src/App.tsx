ff187c83f7cf8c0e73a3b0ba99a3636643b87bb2
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-16 00:33:37 +0100

    create ThemeProvider and store theme to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useState, createContext, useEffect } from "react"
+import HomePage from "./page/homePage";
+
+export const ThemeContext = createContext({} as any)
 function App() {
+
+  const storedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark';
+  const [theme, setTheme] = useState(storedTheme)
+  useEffect(() => {
+    document.documentElement.classList.remove('dark', 'light')
+    document.documentElement.classList.add(storedTheme!)
+  }, [storedTheme])
+  
+
   return (
-    <div className="App">
-      <h1>dExtinctedCoder</h1>
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className={`bg-white text-black min-h-screen dark:bg-black dark:text-white`}>
+        <HomePage />
+      </div>
+    </ThemeContext.Provider>
   )
 }
 
