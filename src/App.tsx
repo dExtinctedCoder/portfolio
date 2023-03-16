ddfe6fc39fa1576a6644026f82bdf9d1552fb715
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-16 21:42:02 +0100

    refactor: migrate app theme from theme context to use redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import { useState, createContext, useEffect } from "react"
+import { useEffect } from "react"
 import HomePage from "./page/homePage";
+import { useAppSelector } from "./store/hooks";
 
-export const ThemeContext = createContext({} as any)
 function App() {
 
-  const storedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'dark';
-  const [theme, setTheme] = useState(storedTheme)
+  const theme = useAppSelector((state: any) => state.ThemeReducer.currentTheme)
   useEffect(() => {
     document.documentElement.classList.remove('dark', 'light')
-    document.documentElement.classList.add(storedTheme!)
-  }, [storedTheme])
+    document.documentElement.classList.add(theme!)
+  }, [theme])
   
 
   return (
-    <ThemeContext.Provider value={{theme, setTheme}}>
-      <div className={`bg-white text-black min-h-screen dark:bg-black dark:text-white`}>
-        <HomePage />
-      </div>
-    </ThemeContext.Provider>
+    <div className={`bg-white text-black min-h-screen dark:bg-black dark:text-white`}>
+      <HomePage />
+    </div>
   )
 }
 
