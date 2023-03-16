2efa204fbb2b7d14a09f0891c0aa9fa81a9e1f52
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-16 22:08:03 +0100

    combine all reducers into a single reducer

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,7 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
+import { menuReducer } from './reducers/menuReducer'
+import { themeReducer } from './reducers/themeReducer'
 
 export const rootReducer = combineReducers({
-
+  ThemeReducer: themeReducer,
+  MenuReducer: menuReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
