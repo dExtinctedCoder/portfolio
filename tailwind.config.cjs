5312babfe2aecddaeebc560b71e491d7c1bcde64
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-17 12:11:48 +0100

    add custom themes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,12 +7,31 @@ module.exports = {
       'white': '#FFFFFF',
       'green-light': '#C8FF00',
       'gold-light': '#E89300',
+      'pink': '#FF0048',
+      'purple': '#B611EE'
     },
     fontFamily: {
       pry: [],
       sec: [],
     },
-    extend: {},
+    extend: {
+      animation: {
+        spinner: 'spinner 3s ease-in-out infinite',
+      },
+      keyframes: {
+        spinner: {
+          '0%, 100%': { 
+            transform: 'rotate(0deg)'
+          },
+          '50%': {
+            transform: 'rotate(180deg) scale(1.175)',
+          },
+          '100%': {
+             transform: 'rotate(360deg)'
+           },
+        }
+      }
+    },
   },
   plugins: [],
 }
