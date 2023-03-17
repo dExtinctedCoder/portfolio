7760008ca5764a51de4e30ba32cd67a1919f6f31
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-17 17:01:27 +0100

    refactor: add header component directly in homepage

--- src/page/homePage.tsx
+++ src/page/homePage.tsx
@@ -1,9 +1,11 @@
 import About from "../components/sections/about"
 import Hero from "../components/sections/hero"
+import Header from "../components/utilities/header"
 
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <Hero />
       <About />
     </div>
