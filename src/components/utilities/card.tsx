32843b73e839c62fefec61e247d58cc75840f5b7
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-17 18:58:32 +0100

    build reuseable card component for portfolio section

--- src/components/utilities/card.tsx
+++ src/components/utilities/card.tsx
@@ -1,4 +1,6 @@
 import { IconType } from "react-icons/lib"
+import { FaLongArrowAltRight } from "react-icons/fa"
+import {Link} from 'react-router-dom'
 
 export type cardPropsType = {
   Icon: IconType
@@ -10,6 +12,36 @@ const Card = ({Icon, Title, ...props}: cardPropsType) => {
     <div className="w-48 bg-[#bbee03] aspect-square flex items-center justify-center gap-y-6 shadow-lg rounded-lg flex-col dark:bg-[#292929] md:w-52">
       <Icon fontSize={38} className="text-purple dark:text-green-light" />
       <span className="uppercase text-lg text-purple dark:text-green-light">{Title}</span>
+    </div>
+  )
+}
+
+type portfolioPropsType = {
+  title: string
+  detail: string
+  href: string
+  bg: string
+}
+
+export const PortfolioCard = ({title, detail, href, bg}: portfolioPropsType) => {
+  return (
+    <div 
+      className="text-center relative rounded-2xl overflow-hidden flex flex-col items-center justify-center aspect-[3/2] lg:aspect-square"
+    >
+      <div
+        style={{backgroundImage: `url(${bg})`}}
+        className="absolute top-0 left-0 w-full h-full bg-cover bg-no-repeat opacity-30">
+
+      </div>
+      <div className="w-full h-full flex flex-col justify-center items-center p-8 px-10 z-10 text-white bg-black hover:bg-[rgba(22,22,22,0.55)] dark:bg-[#171616] dark:hover:bg-[rgba(22,22,22,0.55)]">
+        <h5 className="text-2xl font-bold text-green-light mb-2">{title}</h5>
+        <p className="mb-5 text-xl font-semibold">{detail}</p>
+        <Link 
+          to={href}
+          target="blank"
+          className="bg-gold-light text-black border-4 border-gold-light py-1 px-4 flex items-center rounded-lg hover:bg-black hover:text-gold-light hover:border-black"
+        >See More <FaLongArrowAltRight className="ml-3" /></Link>
+      </div>
     </div>
   )
 }
