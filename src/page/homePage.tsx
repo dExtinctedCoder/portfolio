febfff76c55bcbcb8825e28f3a94eecfdf9fbb02
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-17 21:28:45 +0100

    add experiience component

--- src/page/homePage.tsx
+++ src/page/homePage.tsx
@@ -2,6 +2,7 @@ import About from "../components/sections/about"
 import Hero from "../components/sections/hero"
 import Portfolio from "../components/sections/portfolio"
 import Header from "../components/utilities/header"
+import Experience from "../components/sections/experience"
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Hero />
       <About />
       <Portfolio />
+      <Experience />
     </div>
   )
 }
