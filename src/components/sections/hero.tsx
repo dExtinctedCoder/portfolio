141e2164f82255062188ab214dfc8d219d491939
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-18 16:52:20 +0100

    add animation delay to spinning tiles

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -23,16 +23,16 @@ const Hero = () => {
             </Link>
           </div>
           <div className="absolute w-full h-full flex flex-wrap grow-0 justify-between gap-x-6 gap-y-6 items-center left-0 top-0 p-16 px-4">
-            <span className={`w-20 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-start`}></span>
+            <span className={`w-20 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-10 border-[6px] opacity-20 self-start`}></span>
             <span className={`w-8 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-start`}></span>
             <span className={`w-12 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-end`}></span>
-            <span className={`w-20 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-start lg:block`}></span>
-            <span className={`w-6 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-center`}></span>
-            <span className={`w-20 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-center`}></span>
-            <span className={`w-8 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-start lg:block`}></span>
-            <span className={`w-12 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-end lg:block`}></span>
-            <span className={`w-20 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-center lg:block`}></span>
-            <span className={`w-12 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-end`}></span>
+            <span className={`w-20 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-20 border-[6px] opacity-20 self-start lg:block`}></span>
+            <span className={`w-6 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-10 border-[6px] opacity-20 self-center`}></span>
+            <span className={`w-20 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-20 border-[6px] opacity-20 self-center md:delay-0`}></span>
+            <span className={`w-8 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-30 border-[6px] opacity-20 self-start lg:block`}></span>
+            <span className={`w-12 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-20 border-[6px] opacity-20 self-end lg:block`}></span>
+            <span className={`w-20 hidden aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-40 border-[6px] opacity-20 self-center lg:block`}></span>
+            <span className={`w-12 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner delay-10 border-[6px] opacity-20 self-end`}></span>
             <span className={`w-8 aspect-square ${theme === 'light' ? 'border-purple' : 'border-pink'} animate-spinner border-[6px] opacity-20 self-start`}></span>
           </div>
         </div>
@@ -43,7 +43,6 @@ const Hero = () => {
           <a href="https://www.linkedin.com/in/dextinctedcoder/" target='_blank'><BsLinkedin fontSize={24} className={`text-black hover:text-purple dark:text-white dark:hover:text-green-light mb-3 cursor-pointer md:mb-6`} /></a>
           <a href="https://wa.me/2347084195933?text=Hi,Jamiu.%I%will%like%to%discuss%a%job%offer%with%you" target='_blank'><BsWhatsapp fontSize={24} className={`text-black hover:text-purple dark:text-white dark:hover:text-green-light cursor-pointer`} /></a>
         </div>
-        <div></div>
       </div>
     </section>
   )
