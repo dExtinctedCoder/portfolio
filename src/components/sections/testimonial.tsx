e9f9c41f4d461295653e68da09abe04819cc9752
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2023-03-18 16:52:55 +0100

    fix: increase auto scroll time in slider

--- src/components/sections/testimonial.tsx
+++ src/components/sections/testimonial.tsx
@@ -10,7 +10,7 @@ const Testimonial = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 4000,
     pauseOnHover: true
   };
 
@@ -20,7 +20,7 @@ const Testimonial = () => {
       <Slider autoplay {...settings}>
         <div className="relative px-2">
           <p className="text-xl max-w-[60ch]">
-            <p className="mb-12 relative pb-3 leading-7">Jamiu is a seasoned developer with reasonable amount of experience in Front-End development. He led and managed our Front-End development unit during the 2022 Zuri Internship which we ranked among the top 10 best teams. Throughout the project, he was able to take our vision and turn it into a beautiful and functional website. His attention to detail and understanding of user experience made for a website that was both aesthetically pleasing and easy to use.
+            <p className="mb-12 relative pb-3 leading-7">Jamiu is a seasoned developer with reasonable amount of experience in Front-End development. He led and managed our Front-End development unit during the <span className="text-purple font-medium dark:text-green-light">2022 Zuri Internship</span> which we ranked among the <span className="text-purple font-medium dark:text-green-light">top 10</span> best teams. Throughout the project, he was able to take our vision and turn it into a beautiful and functional website. His <span className="text-purple font-medium dark:text-green-light">attention to detail</span> and understanding of <span className="text-purple font-medium dark:text-green-light">user experience</span> made for a website that was both <span className="text-purple font-medium dark:text-green-light">aesthetically pleasing</span> and <span className="text-purple font-medium dark:text-green-light">easy to use</span>.
               <span className="absolute right-0 bottom-0 w-1/2 h-[3px] bg-gradient-to-r from-pink to-purple dark:from-gold-light dark:to-pink md:w-1/3"></span>
             </p>
             <b className="mb-1 leading-6 capitalize block">Abdullahi Busari O</b>
@@ -32,7 +32,7 @@ const Testimonial = () => {
         </div>
         <div className="relative px-2">
           <p className="text-xl max-w-[60ch]">
-            <p className="mb-12 relative pb-3 leading-7">I had the pleasure of working with Jamiu on a project, and I have to say, it was an excellent experience. He was not only highly skilled and knowledgeable in frontend development but also a great team player and communicator. I highly recommend Jamiu for any frontend development projects.
+            <p className="mb-12 relative pb-3 leading-7">I had the pleasure of working with Jamiu on a project, and I have to say, it was an excellent experience. He was not only <span className="text-purple font-medium dark:text-green-light">highly skilled</span> and knowledgeable in frontend development but also a <span className="text-purple font-medium dark:text-green-light">great team player</span> and communicator. I highly <span className="text-purple font-medium dark:text-green-light">recommend</span> Jamiu for any frontend development position.
               <span className="absolute right-0 bottom-0 w-1/2 h-[3px] bg-gradient-to-r from-pink to-purple dark:from-gold-light dark:to-pink md:w-1/3"></span>
             </p>
             <b className="mb-1 leading-6 capitalize block">Al-Hassan Abdulmumin</b>
